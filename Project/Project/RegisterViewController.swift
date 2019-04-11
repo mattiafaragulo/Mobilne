3c0651280c13f10a9e158a14527ea9c4791523e7
MattiaDracoFaragulo97 <48323325+MattiaDracoFaragulo97@users.noreply.github.com>
2019-04-11 09:24:32 +0200

    Add files via upload

---
+++ Project/Project/RegisterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterViewController.swift
+//  Project
+//
+//  Created by Systemy mobilne on 11/04/2019.
+//  Copyright © 2019 Systemy mobilne. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var user: UITextField!
+    
+    @IBOutlet weak var p: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        user.placeholder = "username"
+        p.placeholder = "password"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project/Project/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Systemy mobilne on 11/04/2019.
+//  Copyright © 2019 Systemy mobilne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        username.placeholder = "username"
+        password.placeholder = "password"
+    }
+
+
+}
+
